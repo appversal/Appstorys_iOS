e0090af7ab0c2d3ed82382e54ed5bba7c812dcb9
Suraj Kumar <118191630+Surajkumar2903@users.noreply.github.com>
2026-07-11 00:34:13 +0530

    Update AppStorys_iOS to version 1.7.6

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppStorys_iOS",
-            url: "https://github.com/appversal/Appstorys_iOS/releases/download/1.7.5/AppStorys_iOS.xcframework.zip",
-            checksum: "c6a6b81feefad72de399e49ad4b6e03abb95b153406267ddc1707b660193d2be"
+            url: "https://github.com/appversal/Appstorys_iOS/releases/download/1.7.6/AppStorys_iOS.xcframework.zip",
+            checksum: "c8516db751a86b1167a8aa073860953eaeb5d5034fba3bfcbf90765d9a2968ff"
         ),
     ]
 )
